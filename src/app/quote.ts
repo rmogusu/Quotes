cbb52faf15d1ff2352019de1636c0424f9680cad
mrsfreddy <rmogusu123@gmal.com>
2019-09-18 17:18:44 +0300

    Define attributes inside the class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,10 @@
 export class Quote {
+    public upvote: true;
+    showcompleteDate: boolean;
+    constructor(public title: string, public body: string, public author: string, public completeDate: Date, public like: number,
+      public dislike: number) {
+      this.showcompleteDate = false;
+    }
+  
+
 }
