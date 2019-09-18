87e9e1737c81a62fe4158e028f2233c913f139f5
mrsfreddy <rmogusu123@gmal.com>
2019-09-18 18:55:33 +0300

    Define my quotes using new key word and call quote blueprint class inside itspecifying mandatory properties inside the constructor

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,15 +8,13 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {  
   quotes: Quote[] = [
 
-  new Quote('Success', 'Success usually comes to those who are too busy to be looking for it.', 'Henry David Thoreau', new Date(2019, 8 - 1, 23),0,0),
+ new Quote('Success', 'Success usually comes to those who are too busy to be looking for it.', 'Henry David Thoreau', new Date(2019, 8 - 1, 23),0,0),
   new Quote('Failure ', 'Giving up is the only sure way to fail.', 'Gena Showalter', new Date(2019, 8 - 1, 15),0,0),
   new Quote('Fear', 'Everything you want is on the other side of fear.', 'Jack Canfield', new Date(2019, 8 - 1, 12),0,0),
   new Quote('Wisdom', 'Knowing yourself is the beginning of all wisdom.', 'Aristotle', new Date(2019, 8 - 1, 10),0,0),
   new Quote('Passion ', 'Passion is the genesis of genius.', 'Tony Robbins', new Date(2019, 8 - 1, 19),0,0),
-  new Quote('Investment ', 'Never invest in a business you cannot understand.', 'Warren Buffett', new Date(2019, 8 - 1, 17),0,0)
+  new Quote('Investment ', 'Never invest in a business you cannot understand.', 'Warren buffett', new Date(2019, 8 - 1, 17),0,0)
 ];
-
-
   constructor() { }
 
   ngOnInit() {
