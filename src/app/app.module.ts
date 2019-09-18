78669617ba6cf59f2c1e0bb7bbe02b3d08255aaa
mrsfreddy <rmogusu123@gmal.com>
2019-09-18 17:07:48 +0300

    Generate parent component ,child component and class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
