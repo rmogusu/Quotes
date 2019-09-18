e6dedfe991d3dd7f1ffb76b74fd383857e7aadd5
mrsfreddy <rmogusu123@gmal.com>
2019-09-18 18:04:24 +0300

    Nest my parent  component inside root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailsComponent,
     QuoteDetailComponent
   ],
   imports: [
