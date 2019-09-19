e4cf5b35f266f6b33a932e0588022c6feffa9cdf
mrsfreddy <rmogusu123@gmal.com>
2019-09-19 13:41:57 +0300

    Add newQuote function that takes quotes object as an argument and push the new Quote to our array Quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -18,6 +18,13 @@ export class QuoteComponent implements OnInit {
 toggleDetails(index) {
   this.quotes[index].showCompleteDate = !this.quotes[index].showCompleteDate;
 }
+addNewQuote(quote){
+  let quoteLength = this.quotes.length;
+  // quote.id = quoteLength+1;
+  quote.completeDate = new Date(quote.completeDate)
+  this.quotes.push(quote)
+}
+
 deleteQuote(isComplete, index) {
   if (isComplete) {
     let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].title}?`)
